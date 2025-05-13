d0b9d9cddcea6276a67a58f5d63ab7662930427a
unknown <dionevasconcelos17@gmail.com>
2025-05-13 17:39:23 -0300

    handlers

---
+++ DSCommerce/src/main/java/com/bootcamp/DSCommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.bootcamp.DSCommerce.controllers.handlers;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+
+
+}
