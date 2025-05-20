32e9530cfae2134f36752b067d4cf899fa94a428
unknown <dionevasconcelos17@gmail.com>
2025-05-20 08:43:51 -0300

    AuthService

--- DSCommerce/src/main/java/com/bootcamp/DSCommerce/services/AuthService.java
+++ DSCommerce/src/main/java/com/bootcamp/DSCommerce/services/AuthService.java
@@ -1,6 +1,7 @@
 package com.bootcamp.DSCommerce.services;
 
 import com.bootcamp.DSCommerce.entities.User;
+import com.bootcamp.DSCommerce.services.exceptions.ForbiddenException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class AuthService {
@@ -10,6 +11,8 @@ public class AuthService {
 
     public void validateSelfOrAdmin(long userId) {
         User me = userService.authenticated();
-        if(!me.hasRole("ROLE_ADMIN") && !me.getId().equals(userId)) {}
+        if(!me.hasRole("ROLE_ADMIN") && !me.getId().equals(userId)) {
+            throw new ForbiddenException("Access denied");
+        }
     }
 }
